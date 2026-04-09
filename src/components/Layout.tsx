e0bbe05470a37050af020bc42001d9b9d0ae2389
Håkon <haakmae@stud.ntnu.no>
2026-04-09 19:21:56 +0200

    refactored into Layout.tsx to avoid duplication of code

---
+++ src/components/Layout.tsx
@@ -0,0 +1,78 @@
+import {useEffect, useRef, useState} from "react";
+import {Link, Outlet} from "react-router-dom";
+import HamburgerMenu from "./HamburgerMenu";
+import "../css/HamburgerMenu.css";
+import "../css/Slider.css";
+
+export default function Layout() {
+    const [showTopbar, setShowTopbar] = useState(true);
+    const lastScrollY = useRef(0);
+
+    useEffect(() => {
+        const handleWindowScroll = () => {
+            const currentY = window.scrollY;
+            const isMobile = window.innerWidth <= 600;
+
+
+            if (!isMobile) {
+                setShowTopbar(true);
+                lastScrollY.current = currentY;
+                return;
+            }
+
+            const delta = currentY - lastScrollY.current;
+
+            if (delta > 5 && currentY > 50) {
+                setShowTopbar(false);
+            } else if (delta < -5) {
+                setShowTopbar(true);
+            }
+
+            lastScrollY.current = currentY;
+        };
+
+        window.addEventListener("scroll", handleWindowScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleWindowScroll);
+        };
+    }, []);
+
+
+    return (
+        <div id="root">
+            <div className="main-content">
+
+                {/* Topbar */}
+                <div className={`topnav ${showTopbar ? "topnav--visible" : "topnav--hidden"}`}>
+                    <div className="topnav-left">
+                        <div className="hamburger-menu">
+                            <header>
+                                <HamburgerMenu/>
+                            </header>
+                        </div>
+                    </div>
+                    <div className="topnav-middle">
+                        <Link to="/home" className="active">
+                            Home
+                        </Link>
+                    </div>
+                    <div className="topnav-right">
+                        <Link to="login">
+                            <button className="login-btn">Login</button>
+                        </Link>
+                    </div>
+                </div>
+
+                {/* Page content*/}
+                <Outlet/>
+            </div>
+
+            {/* Footer */}
+            <footer className="footer">
+                <Link to="about">About</Link>
+                <Link to="contact">Contact</Link>
+            </footer>
+        </div>
+    );
+}
