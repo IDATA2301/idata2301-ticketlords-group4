b0a3029f149508a8494e97b8101363f135d7f382
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-22 18:32:52 +0200

    Created event interface to represent information retrieved by the database, of an event.

---
+++ src/util/dtos/Event.ts
@@ -0,0 +1,12 @@
+export default interface Event {
+  eventId: number;
+  eventName: String;
+  host: String;
+  category: Category;
+  eventDateStart: Date;
+  eventDateEnd: Date;
+  eventVenue: EventVenue;
+  eventDescription: String;
+  totalClicks: number;
+
+}
