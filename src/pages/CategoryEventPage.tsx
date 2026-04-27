724aeae5a5ae9fc67ef4885923f5a2773f89ab62
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-27 16:35:56 +0200

    refactored SearchPage into EventListPage, which can also show events for categories, not just searches.

---
+++ src/pages/CategoryEventPage.tsx
@@ -0,0 +1,32 @@
+import EventListPage from "./EventListPage";
+import { useParams } from "react-router-dom";
+import type Event from "../util/dtos/Event";
+
+
+export default function CategoryEventPage() {
+  const params = useParams<{ categoryName: string }>();
+  const category = params.categoryName;
+
+  const prettyCategoryName = (categoryName: string) => {
+    if (!categoryName) return "";
+    if (categoryName.includes("-")) {
+      categoryName = categoryName.replaceAll("-", " ");
+    }
+    return categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+  }
+  const categoryTitle: string = prettyCategoryName(category ?? "");
+
+  const fetchEvents = async (): Promise<Event[]> => {
+    if (!category) return [];
+    const response = await fetch("http://10.212.25.185:8080/events/category/" + encodeURIComponent(category));
+    if (!response.ok) return [];
+    return response.json();
+  }
+
+  return (
+    <EventListPage
+      title={categoryTitle ?? ""}
+      fetchEvents={fetchEvents}
+    />
+  );
+}
