3982db8a738ffabef492791c9b158c5a55a62028
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-28 15:20:40 +0200

    Formatting.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,90 +1,90 @@
-import {useEffect, useRef, useState} from "react";
-import {Link, Outlet, useLocation} from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
 import "../css/HamburgerMenu.css";
 import "../css/Slider.css";
 
 export default function Layout() {
-    const [showTopbar, setShowTopbar] = useState(true);
-    const lastScrollY = useRef(0);
-    const location = useLocation();
+  const [showTopbar, setShowTopbar] = useState(true);
+  const lastScrollY = useRef(0);
+  const location = useLocation();
 
-    useEffect(() => {
-        const handleWindowScroll = () => {
-            const currentY = window.scrollY;
-            const isMobile = window.innerWidth <= 600;
+  useEffect(() => {
+    const handleWindowScroll = () => {
+      const currentY = window.scrollY;
+      const isMobile = window.innerWidth <= 600;
 
-            if (!isMobile) {
-                setShowTopbar(true);
-                lastScrollY.current = currentY;
-                return;
-            }
+      if (!isMobile) {
+        setShowTopbar(true);
+        lastScrollY.current = currentY;
+        return;
+      }
 
-            const delta = currentY - lastScrollY.current;
+      const delta = currentY - lastScrollY.current;
 
-            if (delta > 5 && currentY > 50) {
-                setShowTopbar(false);
-            } else if (delta < -5) {
-                setShowTopbar(true);
-            }
+      if (delta > 5 && currentY > 50) {
+        setShowTopbar(false);
+      } else if (delta < -5) {
+        setShowTopbar(true);
+      }
 
-            lastScrollY.current = currentY;
-        };
+      lastScrollY.current = currentY;
+    };
 
-        window.addEventListener("scroll", handleWindowScroll);
+    window.addEventListener("scroll", handleWindowScroll);
 
-        return () => {
-            window.removeEventListener("scroll", handleWindowScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleWindowScroll);
+    };
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-        lastScrollY.current = 0;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    lastScrollY.current = 0;
 
-        const timeoutId = window.setTimeout(() => {
-            setShowTopbar(true);
-        }, 0);
+    const timeoutId = window.setTimeout(() => {
+      setShowTopbar(true);
+    }, 0);
 
-        return () => {
-            window.clearTimeout(timeoutId);
-        };
-    }, [location.pathname]);
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [location.pathname]);
 
-    return (
-        <div id="root">
-            <div className="main-content">
+  return (
+    <div id="root">
+      <div className="main-content">
 
-                {/* Topbar */}
-                <div className={`topnav ${showTopbar ? "topnav--visible" : "topnav--hidden"}`}>
-                    <div className="topnav-left">
-                        <div className="hamburger-menu">
-                            <header>
-                                <HamburgerMenu/>
-                            </header>
-                        </div>
-                    </div>
-                    <div className="topnav-middle">
-                        <Link to="/home" className="active">
-                            Home
-                        </Link>
-                    </div>
-                    <div className="topnav-right">
-                        <Link to="/login">
-                            <button className="login-btn">Login</button>
-                        </Link>
-                    </div>
-                </div>
-
-                {/* Page content*/}
-                <Outlet/>
+        {/* Topbar */}
+        <div className={`topnav ${showTopbar ? "topnav--visible" : "topnav--hidden"}`}>
+          <div className="topnav-left">
+            <div className="hamburger-menu">
+              <header>
+                <HamburgerMenu />
+              </header>
             </div>
-
-            {/* Footer */}
-            <footer className="footer">
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-            </footer>
+          </div>
+          <div className="topnav-middle">
+            <Link to="/home" className="active">
+              Home
+            </Link>
+          </div>
+          <div className="topnav-right">
+            <Link to="/login">
+              <button className="login-btn">Login</button>
+            </Link>
+          </div>
         </div>
-    );
+
+        {/* Page content*/}
+        <Outlet />
+      </div>
+
+      {/* Footer */}
+      <footer className="footer">
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </footer>
+    </div>
+  );
 }
