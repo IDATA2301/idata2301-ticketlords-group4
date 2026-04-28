e15167c2a4585cff89ac3037775dfa557b92bda1
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-28 17:00:42 +0200

    Added contact us page, with slight styling

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,32 @@
+import "../css/ContactPage.css";
+
+export default function ContactPage() {
+
+
+  return (
+    <div className="contact-page">
+
+
+      <div className="contact-header">
+        <h1>Contact</h1>
+        <p>Do you have questions? Sorry, we cannot help you...</p>
+      </div>
+
+      <div className="contact-content">
+        <h3>Support</h3>
+        <p>We do not have support available, however if you want you can send a mail to support@ticketlords.no</p>
+
+        <h3>Address</h3>
+        <p>Nye pizza la italia AS</p>
+        <p>Jernbanegata 9</p>
+        <p>2335 Stange</p>
+
+        <h3>Company info</h3>
+        <p>TicketLords are not actively driven by anyone.</p>
+        <p>Organisation number: 67 420 1337 626</p>
+      </div>
+
+
+    </div>
+  )
+}
