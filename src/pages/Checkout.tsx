2865cd8b8a9f9d263e4a521f62e24320eaa07e0f
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-29 13:00:53 +0200

    Start of checkout page

---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Checkout() {
+
+
+
+  return (
+    <div className="checkout-page">
+      <div className="checkout-top">Cart</div>
+      <div className="checkout-left-side"></div>
+      <div className="checkout-right-side"></div>
+    </div>
+  )
+}
