d2eb87d6812bacbd77a26f157d208b5c79d3aeb8
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-05-04 18:24:00 +0200

    Render CartSummary

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
+import { getCart, getCartTotalCost, getTicketCountInCart } from "../functions/CartHandler";
+import CartSummary from "../components/CartSummary";
 
 export default function CheckoutPage() {
 
   const [email, setEmail] = useState("user@example.com");
+
+  const cartItems = getCart().items;
+  const totalCost = getCartTotalCost();
+  const pricePreTax = totalCost * 0.75;
+  const taxPrice = totalCost * 0.25;
+  const itemCount = cartItems.reduce((sum, item) => sum + item.amount, 0);
 
 
 
@@ -10,6 +18,11 @@ export default function CheckoutPage() {
     <div className="checkout-page">
       <div className="checkout-top">Cart</div>
       <div className="checkout-left-side">
+        <CartSummary showCheckoutButton={false}
+          totalCost={getCartTotalCost()}
+          pricePreTax={getCartTotalCost() * 0.75}
+          taxPrice={getCartTotalCost() * 0.25}
+          itemCount={getTicketCountInCart()} />
         <input
           className="email-field"
           type="email"
