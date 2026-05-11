3e2635edf38afe16535cd1fddd41aeeb74610342
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-05-11 14:11:50 +0200

    emailjs setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
 import App from "./App.tsx";
+import emailjs from "@emailjs/browser"
 
+const emailJsPublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string | undefined;
+
+if (!emailJsPublicKey) {
+  console.warn("EmailJS public key is not defined. Email functionality may not work properly.");
+} else {
+  emailjs.init(emailJsPublicKey);
+}
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
